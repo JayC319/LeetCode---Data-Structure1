652648bd129102c0304a3aae1a4dc7817e69de70
Jay Chen <jwei2001@gmail.com>
2022-02-08 23:40:02 +0800

    update solution

--- 74_search a 2D matrix.cpp
+++ 74_search a 2D matrix.cpp
@@ -4,7 +4,7 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int cursor = 0;
-        if(!m)
+        if(!m || !n)
             return false;
         for(int i = 0; i < m; i++) {
             if (target == matrix[i][n-1] || target == matrix[i][0])
@@ -15,12 +15,20 @@ public:
             }
         }
         
-        for (int i = 0; i < n; i++) {
-            if (target == matrix[cursor][i])
+        // for (int i = 0; i < n; i++) {
+        //     if (target == matrix[cursor][i])
+        //         return true;
+        // }
+        int left = 0, right = n -1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target == matrix[cursor][mid])
                 return true;
+            if(target < matrix[cursor][mid])
+                right = mid;
+            else 
+                left = mid + 1;
         }
-        
-        return false;
-           
+        return false;  
     }
 };
